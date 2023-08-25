65d02dd0d3a28b42d0fc166cdbe12f428c352777
C. Burak Kirici <krcdotbrk@gmail.com>
2023-08-25 02:25:12 +0200

    build: add spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "1.8.22"
     application
     checkstyle
+    id("com.diffplug.spotless") version "6.20.0"
 }
 repositories {
     mavenCentral()
@@ -19,6 +20,11 @@ tasks.withType<Checkstyle>().configureEach {
         xml.required.set(false)
         html.required.set(false)
     }
+}
+spotless {
+  java {
+    googleJavaFormat()
+  }
 }
 application {
     mainClass.set("demo.MainJava") 
